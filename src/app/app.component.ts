a0a76f6ddd8b4dc11c49b9864621aba686664898
Mateo071 <floresmateo2000@gmail.com>
2025-03-10 18:15:58 -0400

    created form and added button styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { RouterModule } from '@angular/router';
   imports: [HomeComponent, RouterModule], // Import HomeComponent
   template: `
     <main>
-      <a [routerLink]="['/']"></a>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
