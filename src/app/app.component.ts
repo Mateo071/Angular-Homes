5b4586722170b978a8804d4c09d47cb2d0fb082f
Mateo071 <floresmateo2000@gmail.com>
2025-03-10 16:52:47 -0400

    added details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent], // Import HomeComponent
+  imports: [HomeComponent, RouterModule], // Import HomeComponent
   template: `
     <main>
+      <a [routerLink]="['/']"></a>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
